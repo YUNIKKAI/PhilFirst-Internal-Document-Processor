408ad9dd48780f36f617ed53392f236819700a77
YUNIKKAI <mabuchiiii27@gmail.com>
2025-08-20 10:32:14 +0800

    initial

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, send_file, render_template_string, jsonify
-import os, tempfile
-from app import create_app
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__, static_folder="static")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
